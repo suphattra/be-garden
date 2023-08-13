d5e4a81d28b21dce85b83afd54befbf4f4f602cf
treenate <ben.cpre@gmail.com>
2023-08-14 01:55:14 +0700

    add INsert

--- src/models/masterData.models.js
+++ src/models/masterData.models.js
@@ -6,11 +6,24 @@ const masterData = new mongoose.Schema(
     code: { type: String, default: null },
     type: { type: String, default: null },
     subType: { type: String, default: null },
+    value1: { type: mongoose.Schema.Types.Mixed, default: null },
+    value2: { type: mongoose.Schema.Types.Mixed, default: null }, // Can be String or Number
+    value3: { type: mongoose.Schema.Types.Mixed, default: null },
+    status: { type: String, default: null },
+    description: { type: String, default: null },  // Added description field
+    createdBy: { type: String, default: null },  // Added createdBy field
+    createdDate: { type: Date, default: null },  // Added createdDate field
+    updatedBy: { type: String, default: null },  // Added updatedBy field
+    updatedDate: { type: Date, default: null }  // Added updatedDate field
+ 
   },
   {
     versionKey: false,
   }
 )
+
+
+
 
 const collectionName = 'masterData'
 module.exports = mongoose.model(collectionName, masterData, collectionName)
--- src/modules/controller/operations.ctrl.js
+++ src/modules/controller/operations.ctrl.js
@@ -1,4 +1,5 @@
 const operationsModels = require("../../models/operations.models");
+const masterData = require("../../models/masterData.models");
 
 exports.list = async function (req, res) {
     //var lov = req.query.lov;
@@ -17,18 +18,22 @@ exports.list = async function (req, res) {
         }
         if(queryStr.employee){
             var employeeCodeArr = queryStr.employee.split('|');
+            filter.employee = {};
             filter.employee.employeeCode = {$in: employeeCodeArr};
         }
         if(queryStr.mainBranch){
             var branchCodeArr = queryStr.mainBranch.split('|');
+            filter.mainBranch = {};
             filter.mainBranch.branchCode = {$in: branchCodeArr};
         }
         if(queryStr.subBranch){
             var subBranchCodeArr = queryStr.subBranch.split('|');
+            filter.subBranch = {};
             filter.subBranch.branchCode = {$in: subBranchCodeArr};
         }
         if(queryStr.operationStatus){
             var statusArr = queryStr.operationStatus.split('|');
+            filter.operationStatus = {};
             filter.operationStatus.code = {$in: statusArr};
         }
        
@@ -85,46 +90,100 @@ exports.findById = async function (req, res) {
     }
 };
 
-
-
-exports.insert = function (req, res) {
-    var id = req.params.id;
-    var filter = {};
-    filter =  req.query
+exports.insert = async function (req, res) {
+    const dataList = req.body.dataList;
     var ret = {
-        responseCode: 200,
-        responseMessage: 'Success'
+        resultCode: 200,
+        resultDescription: 'Success',
+        message : "สร้างบันทึกการทำงานสำเร็จ"
     };
+
     try {
-        ret.data = {user:'admin',role:id};
-        res.json(ret);
-        // Employee.find(filter,{
-        // }, function (err, result) {
-        //     if (err) {
-        //         logger.errorStack(err);
-        //         ret.responseCode = 500;
-        //         ret.responseMessage = 'Fail';
-        //         ret.responseDescription = err.message;
-        //         res.json(ret);
-        //         throw err;
-        //     }else{
-        //         logger.info('get user name lov list size :: ' + result.length);
-        //         if(result.length <= 0 ){
-        //             ret.responseCode = 404;
-        //             ret.responseMessage = 'Data Not Found';
-        //             res.json(ret);
-        //         }else{
-        //             ret.data = result;
-        //             res.json(ret);
-        //         }
-        //     }
+        if(dataList == null || dataList == undefined || dataList.length <= 0){
+            ret.resultCode = 400;
+            ret.message = 'Bad Request';
+            ret.resultDescription = 'DataList is required';
+            res.json(ret);
+            return;
+        }
+
+      
+        //check duplicate
+        for (let i = 0; i < dataList.length; i++) {
+            var dataOper = dataList[i];
+            var filter = {};
+            filter.startDate = dataOper.startDate;
+            filter.employee =  { employeeCode : dataOper.employee?.employeeCode };
+            filter.mainBranch = { branchCode : dataOper.mainBranch?.branchCode };
+            filter.task = {code : dataOper.task?.code };
+           // filter.operationStatus = { code : { $in: [ "MD0034","MD0035"] } };
+            filter['operationStatus.code'] = { $in: ["MD0034", "MD0035"] };
+            const result = await operationsModels.find(filter);
+
+            if (result.length > 0) {  
+                ret.resultCode = 400;
+                ret.message = 'มีบันทึกการทำงานนี้อยู่แล้ว';
+                ret.resultDescription = 'Duplicate';
+                res.json(ret);
+                return;
+            }
+        }
+
+        for (let i = 0; i < dataList.length; i++) {
+            var dataOper = dataList[i];
+
             
-        // });
+            var seqOperCode = await masterData.findOne({
+                "type": "SEQ",
+                "subType" : "OPERATION_CODE",
+                "status" : "Active"
+            });
+
+            if(seqOperCode == null || seqOperCode == undefined || seqOperCode.length <= 0){
+                ret.resultCode = 400;
+                ret.message = 'ไม่พบข้อมูล SEQ';
+                ret.resultDescription = 'Not Found';
+                res.json(ret);
+                return;
+            }
+            
+            var operationCode = seqOperCode.value1 + seqOperCode.value2;    //prefix + running number
+            dataOper.operationCode = operationCode;
+
+            //update seq
+            var seqOperCodeUpdate = await masterData.updateOne(
+                {
+                    "type": "SEQ",
+                    "subType": "OPERATION_CODE",
+                    "status": "Active"
+                },
+                { 
+                    $inc: {
+                        "value2": 1
+                    },
+                    $currentDate: {
+                        updatedDate: true
+                    }
+                });
+
+                var now = new Date();
+                dataOper.createdDate = now;
+                dataOper.updatedDate = now;
+                dataOper.updatedBy = dataOper.createdBy;
+
+            //dataOper._id = new mongoose.Types.ObjectId();
+            const newOperation = new operationsModels(dataOper);
+            await newOperation.save();
+        }
+       
+
+        ret.data = {};
+        res.json(ret);
       
     } catch (error) {
-        ret.responseCode = 500;
-        ret.responseMessage = 'Fail';
-        ret.responseDescription = error.message;
+        ret.resultCode = 500;
+        ret.message = 'ระบบเกิดข้อผิดพลาด';
+        ret.resultDescription = "System error :" +error.message;
         res.json(ret);
     }
 };
