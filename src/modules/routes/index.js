ccd26fd9ac7b0aa58ea4493062909825dc2d542d
suphattra <suphattra.s@entronica.co.th>
2023-08-14 01:27:06 +0700

    edit router v1

--- src/modules/routes/index.js
+++ src/modules/routes/index.js
@@ -6,9 +6,9 @@ var masterCtrl = require('../controller/master.ctrl');
 router.get('/api/login',login.login);
 router.get('/api/employee',employeeCtrl.employee);
 router.get('/api/employee/:id',employeeCtrl.employeeById);
-router.get('/api/masters',masterCtrl.master);
+router.get('/api/v1/masters',masterCtrl.master);
 
 const operationRoute = require('./operations.route');
-router.use('/api/operations', operationRoute);
+router.use('/api/v1/operations', operationRoute);
 
 module.exports = router;
