1499421e4a6be52e2706861093c9926a671507e6
suphattra <suphattra.s@entronica.co.th>
2023-08-14 00:06:43 +0700

    findById operation

--- src/modules/controller/operations.ctrl.js
+++ src/modules/controller/operations.ctrl.js
@@ -1,3 +1,5 @@
+const operationsModels = require("../../models/operations.models");
+
 exports.list = function (req, res) {
     //var lov = req.query.lov;
     var filter = {};
@@ -42,39 +44,34 @@ exports.list = function (req, res) {
 };
 
 
-exports.findById = function (req, res) {
+exports.findById = async function (req, res) {
     var id = req.params.id;
     var filter = {};
     filter =  req.query
+    filter.operationCode = id
     var ret = {
         responseCode: 200,
         responseMessage: 'Success'
     };
     try {
-        ret.data = {user:'admin',role:id};
+        try {
+            const result = await operationsModels.find(filter);
+
+            if (result.length <= 0) {
+                ret.responseCode = 404;
+                ret.responseMessage = 'Data Not Found';
+            } else {
+                ret.data = result;
+            }
+
+        } catch (error) {
+            ret.responseCode = 500;
+            ret.responseMessage = 'Fail';
+            ret.responseDescription = error.message;
+        }
+
         res.json(ret);
-        // Employee.find(filter,{
-        // }, function (err, result) {
-        //     if (err) {
-        //         logger.errorStack(err);
-        //         ret.responseCode = 500;
-        //         ret.responseMessage = 'Fail';
-        //         ret.responseDescription = err.message;
-        //         res.json(ret);
-        //         throw err;
-        //     }else{
-        //         logger.info('get user name lov list size :: ' + result.length);
-        //         if(result.length <= 0 ){
-        //             ret.responseCode = 404;
-        //             ret.responseMessage = 'Data Not Found';
-        //             res.json(ret);
-        //         }else{
-        //             ret.data = result;
-        //             res.json(ret);
-        //         }
-        //     }
-            
-        // });
+
       
     } catch (error) {
         ret.responseCode = 500;
