07019f45b3a4558e753970133d5476cfa1f345f4
suphattra <suphattra.s@entronica.co.th>
2023-08-19 16:02:24 +0700

    edit master

--- src/modules/controller/master.ctrl.js
+++ src/modules/controller/master.ctrl.js
@@ -6,64 +6,25 @@ exports.master = async function (req, res) {
     filter = req.query
     console.log(req.query);
     var ret = {
-        responseCode: 200,
-        responseMessage: 'Success'
+        resultCode: 200,
+        resultDescription: 'Success'
     };
 
     try {
         const result = await masterData.find(filter);
 
         if (result.length <= 0) {
-            ret.responseCode = 404;
-            ret.responseMessage = 'Data Not Found';
+            ret.resultCode = 404;
+            ret.resultDescription = 'Data Not Found';
         } else {
-            ret.data = result;
+            ret.resultData = result;
         }
         
     } catch (error) {
-        ret.responseCode = 500;
-        ret.responseMessage = 'Fail';
-        ret.responseDescription = error.message;
+        ret.resultCode = 500;
+        ret.message = 'Fail';
+        ret.resultDescription = error.message;
     }
 
     res.json(ret);
-    // try {
-    //     var result = await masterData.find(filter ,{},).then(function (err, result) {
-    //         console.log(err)
-    //         if (err) {
-
-    //             // ret.responseCode = 500;
-    //             // ret.responseMessage = 'Fail';
-    //             // ret.responseDescription = err.message;
-    //             //res.json(ret);
-    //             throw err;
-    //         } 
-            
-            
-            
-    //         else {
-    //             if (result.length <= 0) {
-    //                 ret.responseCode = 404;
-    //                 ret.responseMessage = 'Data Not Found';
-
-    //                 throw err;
-    //                 //res.json(ret);
-    //             } else {
-    //                 ret.data = result;
-    //                 return ret;
-    //                 //res.json(ret);
-    //             }
-    //         }
-           
-    //         //Return results
-    //     });
-
-    //     res.json(result);
-
-    // } catch (error) {
-    //     ret.responseCode = 500;
-    //     ret.responseMessage = 'Fail';
-    //     ret.responseDescription = error.message;
-    //     res.json(ret);
-    // }
 };
